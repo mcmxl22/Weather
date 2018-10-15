c4aa2ad110ffca26c54bd058bccbf4977b1dabf7
Micah M <mcmxl@reagan.com>
2018-10-14 18:16:21 -0600

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
 '''By Micah M. 2018
-   forecast version 1.1
+   forecast version 1.2
    Python 3.7'''
 
+
 def forecast():
-    '''Forecasts based on barometric pressure trends
-       and logs which trend is chosen.'''
+    '''Forecast based on barometric pressure trends
+       and log the trend chosen.'''
     trend_options = ['1 Rising', '2 Falling', '3 Steady']
     print('\n'.join(trend_options))
-
-    trend = input('\nChoose a trend.\n> ')
-    log_entry = trend  # Logs chosen option.
-    file = open('trendLog.txt', 'a')
-    file.write(log_entry)  # Writes log to file.
-    read_log = open('trendLog.txt').read()  # Reads log from file.
-    most_common = max(read_log, key=read_log.count) # Finds most common log entry.
-    file.close()
-    print(f'\nYour most common choice is {most_common}.')
+    trend = input('Choose a trend. ')
 
     if trend == '1':
         print('\nFairer weather on the way.\n')
