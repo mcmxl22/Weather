e4fc3033273a337c450498f361ba8f50ba788bbe
Micah M <mcmxl@reagan.com>
2018-10-21 00:24:54 -0600

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 '''By Micah M. 2018
-   Weather version 10
+   Weather version 10.1
    Python 3.7
    Requires forecast.py, cloud_types.py, entry_log.py'''
 
@@ -10,37 +10,39 @@ import entry_log
 
 DEGREE_SIGN = u'\N{DEGREE SIGN}'
 
+
 def dew_point():
     '''Calculate the dew point.'''
     temperature = input('\nEnter the temperature in Celsius. ')
     relative_humidity = input('\nEnter the relative humidity ')
     dew_point_formula = (int(temperature) - ((100 - int(relative_humidity)) / 5))
-    print(f'\nThe dew point is {int(dew_point_formula)} degrees Celsius.\n')
+    print(f'\nThe dew point is {int(dew_point_formula)}{DEGREE_SIGN}C.\n')
+
 
 def cloud_base():
     '''Calculate the height of the clouds.'''
-    temperature = input('Enter the temperature in celsius. ')
-    dew_pnt = input('Enter dew point in celsius. ')
-    spread = int(temperature) - int(dew_pnt)
+    temperature = input('Enter the temperature in Celsius. ')
+    enter_dew_point = input('Enter dew point in Celsius. ')
+    spread = int(temperature) - int(enter_dew_point)
     cloud_ceiling_formula = int(spread) / 2.5 * 1000
-    print(f'''The cloud ceiling is
-              \r{int(cloud_ceiling_formula)} feet above the ground.\n''')
+    print('The cloud ceiling is')
+    print(f'{int(cloud_ceiling_formula)}\' above the ground.\n''')
 
 
 def celsius():
     '''Convert fahrenheit to celsius.'''
     celsius_convert = input('\nEnter temperature in Celsius. ')
     conversion_formula = int(celsius_convert) * 1.8 + 32
-    print(f'''{celsius_convert}{DEGREE_SIGN} Celsius is
-              \r{int(conversion_formula)}{DEGREE_SIGN} Fahrenheit.\n''')
+    print(f'{celsius_convert}{DEGREE_SIGN}C is')
+    print(f'{int(conversion_formula)}{DEGREE_SIGN}F.\n')
 
 
 def fahrenheit():
     '''Convert celsius to fahrenheit.'''
     fahrenheit_convert = input('\nEnter the temperature in Fahrenheit. ')
     conversion_formula = (int(fahrenheit_convert) - 32) / 1.8
-    print(f'''{fahrenheit_convert}{DEGREE_SIGN} Fahrenheit is 
-              \r{int(conversion_formula)}{DEGREE_SIGN} Celsius.\n''')
+    print(f'{fahrenheit_convert}{DEGREE_SIGN}F is')
+    print(f'{int(conversion_formula)}{DEGREE_SIGN}C.\n')
 
 
 def wind_speed():
@@ -70,14 +72,14 @@ def prompt():
         elif prompt_choice == '4':
             forecast = [sys.executable, 'forecast.py']
             subprocess.run(forecast)
-        elif prompt_choice  == '5':
+        elif prompt_choice == '5':
             cloud_base()
-        elif prompt_choice  == '6':
+        elif prompt_choice == '6':
             wind_speed()
-        elif prompt_choice  == '7':
+        elif prompt_choice == '7':
             cloud_types = [sys.executable, 'cloud_types.py']
             subprocess.run(cloud_types)
-        elif prompt_choice  == '8':
+        elif prompt_choice == '8':
             raise SystemExit
         else:
             print('\nInvalid Entry\n')
