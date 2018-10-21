88d639809dab148b5911fb9a31cf21550e943199
Micah M <mcmxl@reagan.com>
2018-10-20 23:46:08 -0600

    Update entry_log.py

--- entry_log.py
+++ entry_log.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 '''By Micah M. 2018
-   entry_log version 1.1
+   entry_log version 1.2
    Python 3.7'''
 
 import os
 
 def entry(unit_choice):
     '''Log user entry and indicate most common entry.'''
-    log_entry = unit_choice
-    file = open('promptLog.txt', 'a')
-    file.write(log_entry)
-    read_log = open('promptLog.txt').read()
-    if os.stat('promptLog.txt').st_size == 0:
-        print('No choices made yet.')
-    else:
-        most_common = max(read_log, key=read_log.count) # Finds most common entry.
-        print(f'\nYour most common choice: {most_common}.')
-    file.close()
+    if unit_choice != '8':
+        log_entry = unit_choice
+        file = open('prompt_log.txt', 'a')
+        file.write(log_entry)
+        read_log = open('prompt_log.txt').read()
+        if os.stat('prompt_log.txt').st_size == 0:
+            print('No choices made.')
+        else:
+            most_common = max(read_log, key=read_log.count)
+            print(f'\nYour most common choice: {most_common}.')
+        file.close()
 
 
 if __name__ == "__main__":
