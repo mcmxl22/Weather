abecd305f74ea5106bfab50806a77143485a77a0
Micah M <mcmxl@reagan.com>
2018-11-04 13:54:58 -0700

    Update extended_forecast.py

--- extended_forecast.py
+++ extended_forecast.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
-   extended_forecast version 1
-   Python 3.7'''
+   extended_forecast version 1.1
+   Python 3.7.1'''
 
 from weather import Weather, Unit
 
 DEGREE_SIGN = u'\N{DEGREE SIGN}'
 
 
-def extended_forecast():
+def forecast(city):
     city = input('Enter your location. ')
 
     weather = Weather(unit = Unit.FAHRENHEIT)
@@ -17,11 +17,11 @@ def extended_forecast():
     forecasts = location.forecast
 
     for forecast in forecasts:
-        print(forecast.date)
+        print(f'\n{forecast.date}')
         print(forecast.text)
         print(f'{forecast.high}{DEGREE_SIGN}F High')
         print(f'{forecast.low}{DEGREE_SIGN}F Low\n')
 
 
 if __name__ == "__main__":
-    extended_forecast()
+    forecast('city')
