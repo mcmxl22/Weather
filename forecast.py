d25be2f243dc6c74869b1b310a48a493cabd271e
Micah M <mcmxl@reagan.com>
2018-11-04 13:26:46 -0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
    forecast version 1.3
-   Python 3.7'''
+   Python 3.7.1'''
 
+import extended_forecast
+import pressure_forecast
 import subprocess
 import sys
 
 
-def forecast():
+def forecast(forecast_choice):
     '''Weather forecasts'''
-    forecast_options = ['1 Pressure trend', '2 Extended']
+    forecast_options = ['1 Pressure trend based', '2 Extended']
     print('\n'.join(forecast_options))
-    forecast_choice = input('\nChoose a forecast. ')
+    forecast_choice = input('Choose a forecast. ')
 
-    if forecast_choice == '1':
+    if forecast_choice in '1':
         this_forecast = [sys.executable, 'pressure_forecast.py']
         subprocess.run(this_forecast)
-    elif forecast_choice == '2':
+    elif forecast_choice in '2':
         this_forecast = [sys.executable, 'extended_forecast.py']
         subprocess.run(this_forecast)
     else:
-        print('Invalid Entry!\n')
+        print('Invalid Entry!')
         forecast()
 
 
