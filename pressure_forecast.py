88969e4b4830ad3111731297212fffba986d1e21
Micah M <mcmxl@reagan.com>
2018-11-04 13:56:25 -0700

    Update pressure_forecast.py

--- pressure_forecast.py
+++ pressure_forecast.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 '''By Micah M. 2018
-   pressure_forecast version 1.2
-   Python 3.7'''
+   pressure_forecast version 1.3
+   Python 3.7.1'''
 
 
-def forecast():
+def forecast(trend):
     '''Forecasts based on barometric pressure trends'''
     trend_options = ['1 Rising', '2 Falling', '3 Steady']
     print('\n'.join(trend_options))
-    trend = input('\nChoose a trend.\n> ')
+    trend = input('\nChoose a trend. ')
 
-    if trend == '1':
-        print('\nFairer weather on the way.\n')
-    elif trend == '2':
-        print('\nPoorer weather on the way.\n')
-    elif trend == '3':
-        print('\nNo significant change.\n')
+    if trend in '1':
+        print('Fairer weather on the way.')
+    elif trend in '2':
+        print('Poorer weather on the way.')
+    elif trend in '3':
+        print('No significant change.\n')
     else:
-        print('Invalid Entry!\n')
-        forecast()
+        print('Invalid Entry!')
+        forecast(trend)
 
 
 if __name__ == "__main__":
-    forecast()
+    forecast('trend')
