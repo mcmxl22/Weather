d78e19f7f2cceac2f4bdec9a1cf73398271f60cd
Micah M <mcmxl@reagan.com>
2018-11-04 20:37:56 -0700

    Update McWeather.py

--- McWeather.py
+++ McWeather.py
@@ -4,47 +4,14 @@
    Python 3.7.1'''
 
 from beaufort import scale_wind
+from celsius_conversion import to_fahrenheit
+from fahrenheit_conversion import to_celsius
+from cloud_base import base
 from cloud_types import clouds
+from dew_point import dew_point
 from entry_log import entry
 from forecast import forecast
 from wind_speed import wind
-
-
-DEGREE_SIGN = u'\N{DEGREE SIGN}'
-
-
-def dew_point():
-    '''Calculate the dew point.'''
-    temperature = input('Enter the temperature in Celsius. ')
-    relative_humidity = input('Enter the relative humidity. ')
-    dew_point_formula = (int(temperature) - ((100 - int(relative_humidity)) / 5))
-    print(f'\nThe dew point is {int(dew_point_formula)}{DEGREE_SIGN}C.')
-
-
-def cloud_base():
-    '''Calculate the height of the clouds.'''
-    temperature = input('Enter the temperature in Celsius. ')
-    enter_dew_point = input('Enter dew point in Celsius. ')
-    spread = int(temperature) - int(enter_dew_point)
-    cloud_ceiling_formula = int(spread) / 2.5 * 1000
-    print('The cloud ceiling is')
-    print(f'{int(cloud_ceiling_formula)}\' above the ground. ')
-
-
-def celsius():
-    '''Convert Fahrenheit to Celsius.'''
-    celsius_convert = input('Enter temperature in Celsius. ')
-    conversion_formula = int(celsius_convert) * 1.8 + 32
-    print(f'{celsius_convert}{DEGREE_SIGN}C is')
-    print(f'{int(conversion_formula)}{DEGREE_SIGN}F.')
-
-
-def fahrenheit():
-    '''Convert Celsius to Fahrenheit.'''
-    fahrenheit_convert = input('Enter the temperature in Fahrenheit. ')
-    conversion_formula = (int(fahrenheit_convert) - 32) / 1.8
-    print(f'{fahrenheit_convert}{DEGREE_SIGN}F is')
-    print(f'{int(conversion_formula)}{DEGREE_SIGN}C.')
 
 
 def prompt(prompt_choice):
@@ -60,15 +27,15 @@ def prompt(prompt_choice):
         entry(prompt_choice)
 
         if prompt_choice in '1':
-            fahrenheit()
+            to_celsius(prompt_choice)
         elif prompt_choice in '2':
-            celsius()
+            to_fahrenheit('prompt_choice')
         elif prompt_choice in '3':
-            dew_point()
+            dew_point(prompt_choice)
         elif prompt_choice in '4':
             forecast(prompt_choice)
         elif prompt_choice in '5':
-            cloud_base()
+            base(prompt_choice)
         elif prompt_choice in '6':
             wind(prompt_choice)
         elif prompt_choice in '7':
