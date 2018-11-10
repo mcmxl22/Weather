77164602922179c715357bbbaf2f662cc56ab705
Micah McConnaughey <mcmxl@hotmail.com>
2018-11-10 14:09:05 -0700

    Minor update.

--- cloud_base.py
+++ cloud_base.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-"""By Micah M. 2018
-   cloud_base version 1
-   Python 3.7.1"""
-
-
-def base(temperature):
-    """Calculate the height of the clouds."""
-    temperature = input('Enter the temperature in Celsius. ')
-    enter_dew_point = input('Enter dew point in Celsius. ')
-    spread = int(temperature) - int(enter_dew_point)
-    cloud_ceiling_formula = int(spread) / 2.5 * 1000
-    print('The cloud ceiling is')
-    print(f'{int(cloud_ceiling_formula)}\' above the ground.\n')
-
-    if __name__ == "__main__":
-        base('temperature')
-=======
 #!/usr/bin/env python
 """By Micah M. 2018
    cloud_base version 1
@@ -34,4 +15,3 @@ def cloud_base(temperature):
 
     if __name__ == "__main__":
         cloud_base('temperature')
->>>>>>> origin/master
