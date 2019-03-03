7d32787f1ff78d60569a071c29912ea2f70f5dec
Micah M <mcmxl@reagan.com>
2019-03-02 21:38:22 -0700

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-"""By Micah M. 2018
-   forecast version 1.3
+"""forecast version 1.4
    Python 3.7.1"""
 
 import extended_forecast
@@ -9,14 +8,12 @@ import pressure_forecast
 
 def forecast(forecast_choice):
     """Weather forecasts"""
-    forecast_options = ['1 Pressure trend based', '2 Extended']
+    forecast_options = ['1 Pressure trend based']
     print('\n'.join(forecast_options))
     forecast_choice = input('Choose a forecast. ')
 
     if forecast_choice in '1':
         pressure_forecast.forecast(forecast_choice)
-    elif forecast_choice in '2':
-        extended_forecast.forecast(forecast_choice)
     else:
         print('Invalid Entry!')
         forecast(forecast_choice)
