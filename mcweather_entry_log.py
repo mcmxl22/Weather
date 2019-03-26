ae024228dbe71443b098930f98b27a73c6d0d871
Micah M <mcmxl@reagan.com>
2019-03-26 00:19:26 -0600

    Update mcweather_entry_log.py

--- mcweather_entry_log.py
+++ mcweather_entry_log.py
@@ -1,25 +1,22 @@
-#!/usr/bin/env python
-"""By Micah M. 2018
-   mcweather_entry_log version 1.4
-   Python 3.7.1"""
+#!/bin/python3
+"""mcweather_entry_log version 1.5
+   Python 3.7.2"""
 
 import os
 
-
 def entry(unit_choice):
-    """Log user entry and indicate most common entry."""
-    if unit_choice != '10':
+    '''Log user entry and indicate most common entry.'''
+    if unit_choice < '10':
         log_entry = unit_choice
-        file = open('prompt_log.txt', 'a')
-        file.write(log_entry)
-        read_log = open('prompt_log.txt').read()
-        if os.stat('prompt_log.txt').st_size == 0:
-            print('No choices made.')
-        else:
-            most_common = max(read_log, key=read_log.count)
-            print(f'\nYour most common choice: {most_common}.')
-        file.close()
+        with open('prompt_log.txt', 'a') as f:
+            f.write(log_entry)
+            read_log = open('prompt_log.txt').read()
+            if os.stat('prompt_log.txt').st_size == 0:
+                print('No choices made.')
+            else:
+                most_common = max(read_log, key=read_log.count)
+                print(f'\nYour most common choice: {most_common}.')
 
 
 if __name__ == "__main__":
-    entry('unit_choice')
+    entry(unit_choice)
