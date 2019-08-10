2a7485c8f5912b486a2c272f2639befd5cb8b9b9
Micah M <mcmxl@reagan.com>
2019-08-10 02:00:31 -0600

    Update dew_point.py

--- dew_point.py
+++ dew_point.py
@@ -1,16 +1,33 @@
-#!/bin/python3
-"""dew_point version 1
-   Python 3.7.2"""
+#!/usr/bin/env python3
+"""
+dew_point version 1.1
+Python 3.7
+"""
+
+import logging
+
+
+logging.basicConfig(filename="dew_point.log", level=logging.DEBUG)
 
 
 def dew_point(temperature):
     """Calculate the dew point."""
-    degree_sign = u'\N{DEGREE SIGN}'
-    temperature = input('Enter the temperature in Celsius. ')
-    relative_humidity = input('Enter the relative humidity. ')
-    dew_point_formula = (int(temperature) - ((100 - int(relative_humidity)) / 5))
-    result = f'\nThe dew point is {int(dew_point_formula)}{degree_sign}C.'
-    print(result)
+    degree_sign = "\N{DEGREE SIGN}"
+    temperature = input("Enter the temperature in Celsius. ")
+    relative_humidity = input("Enter the relative humidity. ")
+    logging.debug(temperature)
+    logging.debug(relative_humidity)
+
+    try:
+        formula = int(temperature) - ((100 - int(relative_humidity)) / 5)
+        result = f"\nThe dew point is {int(formula)}{degree_sign}C."
+        print(result)
+        logging.debug(result)
+
+    except ValueError as error:
+        logging.debug(error)
+        print("Entry must be a number")
+
 
 if __name__ == "__main__":
-    dew_point('temperature')
+    dew_point("temperature")
