f659e5fe898b1e55617d8a7e3e2ffb11a1ec33e8
Micah M <mcmxl@reagan.com>
2019-08-10 02:15:29 -0600

    Update cloud_base.py

--- cloud_base.py
+++ cloud_base.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-"""cloud_base version 1
-   Python 3.7.2"""
+"""
+cloud_base version 1.1
+Python 3.7
+"""
+
+import logging
+
+
+logging.basicConfig(filename="cloud_base.log", level=logging.DEBUG)
 
 
 def base(temperature):
-    """Calculate the height of the clouds."""
-    temperature = input('Enter the temperature in Celsius. ')
-    enter_dew_point = input('Enter dew point in Celsius. ')
-    spread = int(temperature) - int(enter_dew_point)
-    cloud_ceiling_formula = int(spread) / 2.5 * 1000
-    print(f'''The cloud ceiling is
-          \r{int(cloud_ceiling_formula)}\' above the ground.''')
+    """Calculate the altittude of the clouds."""
+    temperature = input("Enter temperature in Celsius. ")
+    enter_dew_point = input("Enter dew point in Celsius. ")
+
+    try:
+        spread = int(temperature) - int(enter_dew_point)
+        formula = int(spread) / 2.5 * 1000
+        print(f"The cloud ceiling is {int(formula)}' above the ground.")
+
+    except ValueError as error:
+        logging.debug(error)
+        print("Entry must be a number.")
 
 
 if __name__ == "__main__":
-    base('temperature')
+    base("temperature")
