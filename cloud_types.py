36af4e8880a6bd247aede63175d07fda5af3aecc
Micah M <mcmxl@reagan.com>
2019-08-11 14:58:46 -0600

    Changed if/else statements to dicts.

--- cloud_types.py
+++ cloud_types.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-cloud_types version 1.3
+cloud_types version 1.4
 Python 3.7
 """
 
@@ -13,50 +13,47 @@ def clouds(options):
 
     cloud_types = ["Cumulus", "Stratus", "Cumulonimbus", "Cirrus"]
     cloud_options = ["Cloud descriptions", "Cloud images"]
-
+    invalid = "Invalid Answer!"
     numli.addnum(cloud_options)
-    options = input("Choose an option. ")
+    cloud_options = input("Choose an option. ")
 
-    if options in "1":
+    if cloud_options in "1":
         numli.addnum(cloud_types)
         description = input("Choose an option. ")
 
-        if description in "1":
-            url = "https://en.wikipedia.org/wiki/Cumulus_cloud"
-            webbrowser.open(url)
-        elif description in "2":
-            url = "https://en.wikipedia.org/wiki/Stratus_cloud"
-            webbrowser.open(url)
-        elif description in "3":
-            url = "https://en.wikipedia.org/wiki/Cumulonimbus_cloud"
-            webbrowser.open(url)
-        elif description in "4":
-            url = "https://en.wikipedia.org/wiki/Cirrus_cloud"
-            webbrowser.open(url)
-        else:
-            print("Invalid Answer!")
+        description_dict = {
+            "1": "https://en.wikipedia.org/wiki/Cumulus_cloud",
+            "2": "https://en.wikipedia.org/wiki/Stratus_cloud",
+            "3": "https://en.wikipedia.org/wiki/Cumulonimbus_cloud",
+            "4": "https://en.wikipedia.org/wiki/Cirrus_cloud"
+        }
 
-    elif options in "2":
+        if description in description_dict:
+            webbrowser.open(description_dict[description])
+
+        else:
+            print(invalid)
+
+
+    elif cloud_options in "2":
         numli.addnum(cloud_types)
-        img = input("Choose an option. ")
+        img_choice = input("Choose an option. ")
 
-        if img in "1":
-            url = "https://bit.ly/2xH55Er"
-            webbrowser.open(url)
-        elif img in "2":
-            url = "https://bit.ly/2xxD42N"
-            webbrowser.open(url)
-        elif img in "3":
-            url = "https://bit.ly/2Nwns9V"
-            webbrowser.open(url)
-        elif img in "4":
-            url = "https://bit.ly/2xMDgKG"
-            webbrowser.open(url)
+        img_dict = {
+            "1": "https://bit.ly/2xH55Er",
+            "2": "https://bit.ly/2xxD42N",
+            "3": "https://bit.ly/2Nwns9V",
+            "4": "https://bit.ly/2xMDgKG"
+        }
+
+        if img_choice in img_dict:
+            webbrowser.open(img_dict[img_choice])
+
         else:
-            print("Invalid Answer!")
+            print(invalid)
 
     else:
-        print("Invalid Answer!")
+        print(invalid)
 
 
 if __name__ == "__main__":
