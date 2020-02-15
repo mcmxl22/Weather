0a9352b2464083f01e7b81392abb305fab305134
Micah M <mcmxl@reagan.com>
2020-02-15 13:13:37 -0700

    Add files via upload

---
+++ plt_test.py
@@ -0,0 +1,14 @@
+from matplotlib import pyplot as plt
+import xlrd
+
+wb = xlrd.open_workbook("C:/Users/User/MyStuff/Temp.xls")
+sheet1 = wb.sheet_by_name("Temperature")
+
+time_x = sheet1.col_values(1)
+temp_y = sheet1.col_values(0)
+plt.title("Temperature by time")
+plt.xlabel("Time")
+plt.ylabel("Temperature")
+plt.plot(time_x, temp_y)
+
+plt.show()
