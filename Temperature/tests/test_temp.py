d91b38056f73bb6c45b27feed671c361fba90178
Micah M <mcmxl@reagan.com>
2020-02-17 02:06:21 -0700

    Update test_temp.py

--- Temperature/tests/test_temp.py
+++ Temperature/tests/test_temp.py
@@ -1,10 +1,12 @@
 import get_temp
 import get_time
 
+
 def tst1():
     gt1 = get_time.get_time()
     assert str(gt1)
     assert len(gt1) == 5
+
 
 def tst2():
     gt2 = get_temp.get_temperature()
