05a392ddffeafa64b56f775f34823c7ee7fadbd5
Micah M <mcmxl@reagan.com>
2020-02-17 02:01:46 -0700

    Update get_temp.py

--- Temperature/get_temp.py
+++ Temperature/get_temp.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """
-get_temp.py version 2
+get_temp.py version 2.1
 Python 3.7
 """
 
 from bs4 import BeautifulSoup
-from datetime import datetime
+import get_time
 import requests
 import time
 from xlwt import Workbook
@@ -26,24 +26,18 @@ def get_temperature():
     return temp
 
 
-def get_time():
-    """Gets time of day."""
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    return current_time
-
-
 def write_temperature():
     """Writes temperature and time to .xls file at set interval."""
-    seconds = 3600
+    seconds = 60
     col, row = 0, 0
     book = Workbook()
     sheet1 = book.add_sheet("Temperature")
     while True:
-        print(f"Writting at: {get_time()}", end="\r")
+        this_time = get_time.get_time()
+        print(f"Temperature recorded at: {this_time}", end="\r")
         try:
             sheet1.write(row, col, get_temperature())
-            sheet1.write(row, col + 1, get_time())
+            sheet1.write(row, col + 1, this_time)
             book.save("Temp.xls")
             row += 1
             time.sleep(seconds)
