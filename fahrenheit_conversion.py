9c11319f9ac8b320ece8eafdab17afd4020c66f2
Micah M <mcmxl@reagan.com>
2020-04-15 09:52:31 -0600

    Update fahrenheit_conversion.py

--- fahrenheit_conversion.py
+++ fahrenheit_conversion.py
@@ -1,14 +1,16 @@
 #!/bin/python3
-"""fahrenheit_conversion version 1
-   Python 3.7.2"""
+"""
+fahrenheit_conversion version 1.1
+Python 3.7
+"""
 
 def to_celsius(celsius_convert):
     """Convert Fahrenheit to Celsius."""
     degree_sign = u'\N{DEGREE SIGN}'
-    celsius_convert = input('Enter temperature in Celsius. ')
-    conversion_formula = int(celsius_convert) * 1.8 + 32
+    celsius_convert = int(input('Enter temperature in Celsius. '))
+    conversion_formula = celsius_convert * 1.8 + 32
     print(f'''{celsius_convert}{degree_sign}C is
-          \r{int(conversion_formula)}{degree_sign}F.''')
+          \r{conversion_formula}{degree_sign}F.''')
 
 if __name__ == "__main__":
     to_celsius('celsius_convert')
