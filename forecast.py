c9f61bc101ad65b8f61170682151c254543c96d8
Micah M <mcmxl@reagan.com>
2020-04-15 09:55:08 -0600

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,14 +1,17 @@
 #!/bin/python3
-"""forecast version 1.3
-   Python 3.7.2"""
+"""
+forecast version 1.4
+Python 3.7
+"""
 
 import pressure_forecast
+from numli import addnum
 
 
 def forecast(forecast_choice):
     """Weather forecasts"""
-    forecast_options = ['1. Pressure trend based']
-    print('\n'.join(forecast_options))
+    forecast_options = ['Pressure trend based']
+    addnum(forcast_options)
     forecast_choice = input('Choose a forecast. ')
 
     if forecast_choice in '1':
