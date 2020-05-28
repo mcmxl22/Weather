09aed8221d30b4247de48b901a1fd20ff7807e98
Micah M <mcmxl@reagan.com>
2020-05-28 01:03:40 -0600

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+
 """
 forecast version 1.4
 Python 3.7
@@ -10,16 +11,16 @@ from numli import addnum
 
 def forecast(forecast_choice):
     """Weather forecasts"""
-    forecast_options = ['Pressure trend based']
+    forecast_options = ["Pressure trend based"]
     addnum(forcast_options)
-    forecast_choice = input('Choose a forecast. ')
+    forecast_choice = input("Choose a forecast. ")
 
-    if forecast_choice in '1':
+    if forecast_choice in "1":
         pressure_forecast.forecast(forecast_choice)
     else:
-        print('Invalid Entry!')
+        print("Invalid Entry!")
         forecast(forecast_choice)
 
 
 if __name__ == "__main__":
-    forecast('forecast_choice')
+    forecast("forecast_choice")
