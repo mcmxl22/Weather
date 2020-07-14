445d35c80c866b14df97871a97aceca4c50a8c76
Micah M <mcmxl@reagan.com>
2020-07-14 02:16:16 -0600

    Update dew_point.py

--- dew_point.py
+++ dew_point.py
@@ -15,19 +15,22 @@ logging.basicConfig(filename="dew_point.log", level=logging.DEBUG)
 def dew_point(temperature):
     """Calculate the dew point."""
     degree_sign = "\N{DEGREE SIGN}"
-    temperature = int(input("Enter the temperature in Celsius. "))
-    relative_humidity = int(input("Enter the relative humidity. "))
-    logging.debug(temperature)
-    logging.debug(relative_humidity)
 
     try:
+        temperature = int(input("Enter the temperature in Celsius. "))
+        logging.debug(temperature)
+        relative_humidity = int(input("Enter the relative humidity. "))
+        logging.debug(relative_humidity)
+
+    except ValueError as error:
+        logging.debug(error)
+        print("Entry must be a number")
+
+    else:
         formula = temperature - ((100 - relative_humidity) / 5)
         result = f"The dew point is {formula}{degree_sign}C."
         print(result)
         logging.debug(result)
-    except ValueError as error:
-        logging.debug(error)
-        print("Entry must be a number")
 
 
 if __name__ == "__main__":
