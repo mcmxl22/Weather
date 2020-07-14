84839b1cb256cb557453bee4a31bdcff86fd315f
Micah M <mcmxl@reagan.com>
2020-07-14 02:19:08 -0600

    Update cloud_base.py

--- cloud_base.py
+++ cloud_base.py
@@ -10,15 +10,18 @@ import sys
 
 def base(temperature):
     """Calculate the altittude of the clouds."""
-    temperature = int(input("Enter temperature in Celsius. "))
-    enter_dew_point = int(input("Enter dew point in Celsius. "))
-
     try:
+        temperature = int(input("Enter temperature in Celsius. "))
+        enter_dew_point = int(input("Enter dew point in Celsius. "))
+
+    except ValueError:
+        print("Entry must be a number.")
+
+    else:
         spread = temperature - enter_dew_point
         formula = spread / 2.5 * 1000
         print(f"The cloud ceiling is {formula}' above the ground.")
-    except ValueError:
-        print("Entry must be a number.")
+
 
 
 if __name__ == "__main__":
