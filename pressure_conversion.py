bf39b617eefc01fde0875def08dbf66ff2f15f9b
Micah M <mcmxl@reagan.com>
2020-07-14 02:27:45 -0600

    Update pressure_conversion.py

--- pressure_conversion.py
+++ pressure_conversion.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
-
 """
-pressure_conversion version 1.1
+pressure_conversion version 1.2
 Python 3.7
 """
 
+import sys
+
+
 def convert_millibars(millibar_convert):
     """Convert millibars to inches of mercury."""
-    millibar_convert = int(input("Enter pressure in millibars. "))
-    conversion_formula = millibar_convert / 33.864
-    print(f"""{millibar_convert} millibars is
-          \r{round(conversion_formula, 2)} inches of mercury.""")
+    try:
+        millibar_convert = int(input('Enter pressure in millibars. '))
+
+    except ValueError:
+        print("Entry must be a number")
+
+    else:
+        conversion_formula = int(millibar_convert) / 33.864
+        return f"{millibar_convert} millibars is {round(conversion_formula, 2)} inches of mercury."
 
 
 if __name__ == "__main__":
-    convert_millibars("millibar_convert")
+    sys.exit(convert_millibars('millibar_convert'))
