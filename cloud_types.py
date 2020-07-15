4aee8bb9a0c96a92294c218b1e1f7b2d7f0323eb
Micah M <mcmxl@reagan.com>
2020-07-15 17:25:14 -0600

    Complete rewrite!
    Added functions:
        make_menu(), 
        view_clouds(),
        get_cloud_information()
        main()

--- cloud_types.py
+++ cloud_types.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-cloud_types version 1.5
+cloud_types version 1.6
+Requires numli.py
 Python 3.7
 """
 
@@ -10,50 +11,65 @@ import webbrowser
 from numli import add_numbers
 
 
-
-def clouds(options):
-    """View information on cloud types."""
+def make_menu():
+    """Make a menu of cloud types."""
     cloud_types = ["Cumulus", "Stratus", "Cumulonimbus", "Cirrus"]
-    cloud_options = ["Cloud descriptions", "Cloud images"]
-    invalid = "Invalid Answer!"
-    add_numbers(cloud_options)
-    cloud_options = input("Choose an option. ")
+    menu = add_numbers(cloud_types)
+    return menu
 
-    if cloud_options in "1":
-        add_numbers(cloud_types)
-        description = input("Choose an option. ")
 
-        description_dict = {
-            "1": "https://en.wikipedia.org/wiki/Cumulus_cloud",
-            "2": "https://en.wikipedia.org/wiki/Stratus_cloud",
-            "3": "https://en.wikipedia.org/wiki/Cumulonimbus_cloud",
-            "4": "https://en.wikipedia.org/wiki/Cirrus_cloud"
-        }
+def view_clouds():
+    """view images of cloud types"""
+    make_menu()
+    img_dict = {
+        1: "https://bit.ly/2xH55Er",
+        2: "https://bit.ly/2xxD42N",
+        3: "https://bit.ly/2Nwns9V",
+        4: "https://bit.ly/2xMDgKG"
+    }
 
-        if description in description_dict:
-            webbrowser.open(description_dict[description])
-        else:
-            print(invalid)
-
-    elif cloud_options in "2":
-        add_numbers(cloud_types)
-        img_choice = input("Choose an option. ")
-
-        img_dict = {
-            "1": "https://bit.ly/2xH55Er",
-            "2": "https://bit.ly/2xxD42N",
-            "3": "https://bit.ly/2Nwns9V",
-            "4": "https://bit.ly/2xMDgKG"
-        }
-
-        if img_choice in img_dict:
-            webbrowser.open(img_dict[img_choice])
-        else:
-            print(invalid)
-
+    try:
+        view = int(input("Choose an option. "))
+    except ValueError:
+        print("Please enter a number.")
+        view_clouds()
     else:
-        print(invalid)
+        webbrowser.open(img_dict[view])
+
+
+def get_cloud_information():
+    """Get informayion about cloud tyoes."""
+    make_menu()
+    cloud_descriptions = {
+    1: "https://en.wikipedia.org/wiki/Cumulus_cloud",
+    2: "https://en.wikipedia.org/wiki/Stratus_cloud",
+    3: "https://en.wikipedia.org/wiki/Cumulonimbus_cloud",
+    4: "https://en.wikipedia.org/wiki/Cirrus_cloud"
+    }
+
+    try:
+        description = int(input("Choose an option. "))
+    except ValueError:
+        print("Please enter a number.")
+        get_cloud_information()
+    else:
+        webbrowser.open(cloud_descriptions[description])
+
+
+def main():
+    """Explore the clouds."""
+    cloud_choices = ["View clouds", "Cloud information"]
+    add_numbers(cloud_choices)
+    choice = int(input("Choose an option. "))
+
+    if choice == 1:
+        view_clouds()
+    elif choice == 2:
+        get_cloud_information()
+    else:
+        print("invalid entry!")
+        main()
 
 
 if __name__ == "__main__":
-    sys.exit(clouds("options"))
+    sys.exit(main())
