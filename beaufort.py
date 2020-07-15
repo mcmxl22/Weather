9f5608b08703262e8dedbbc62b8be6d6c56f7005
Micah M <mcmxl@reagan.com>
2020-07-15 16:09:40 -0600

    Added error handeling and logging.
    Added use of sys.exit().
    Other minor changes.

--- beaufort.py
+++ beaufort.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 """
-beaufort version 1.1
+beaufort version 1.2
 Python 3.7
 """
 
+import sys
 import logging
 
 
@@ -28,41 +29,44 @@ SCALE = {
 }
 
 
-def scale_wind(wind_speed):
+def scale_wind():
     """Print Beaufort scale description of wind speeds."""
-    wind_speed = int(input("Enter wind speed. "))
+    try:
+        wind_speed = int(input("Enter wind speed. "))
+        logging.debug(wind_speed)
 
-    if wind_speed == 0:
-        print(SCALE["0"])
-    elif wind_speed in range(1, 4):
-        print(SCALE["1"])
-    elif wind_speed in range(4, 8):
-        print(SCALE["2"])
-    elif wind_speed in range(8, 13):
-        print(SCALE["3"])
-    elif wind_speed in range(13, 19):
-        print(SCALE["4"])
-    elif wind_speed in range(19, 25):
-        print(SCALE["5"])
-    elif wind_speed in range(25, 32):
-        print(SCALE["6"])
-    elif wind_speed in range(32, 39):
-        print(SCALE["7"])
-    elif wind_speed in range(39, 47):
-        print(SCALE["8"])
-    elif wind_speed in range(47, 55):
-        print(SCALE["9"])
-    elif wind_speed in range(55, 64):
-        print(SCALE["10"])
-    elif wind_speed in range(64, 73):
-        print(SCALE["11"])
-    elif wind_speed > 72:
-        print(SCALE["12"])
-    else:
-        print("Invalid Entry!")
+        if wind_speed == 0:
+            print(SCALE["0"])
+        elif wind_speed in range(1, 4):
+            print(SCALE["1"])
+        elif wind_speed in range(4, 8):
+            print(SCALE["2"])
+        elif wind_speed in range(8, 13):
+            print(SCALE["3"])
+        elif wind_speed in range(13, 19):
+            print(SCALE["4"])
+        elif wind_speed in range(19, 25):
+            print(SCALE["5"])
+        elif wind_speed in range(25, 32):
+            print(SCALE["6"])
+        elif wind_speed in range(32, 39):
+            print(SCALE["7"])
+        elif wind_speed in range(39, 47):
+            print(SCALE["8"])
+        elif wind_speed in range(47, 55):
+            print(SCALE["9"])
+        elif wind_speed in range(55, 64):
+            print(SCALE["10"])
+        elif wind_speed in range(64, 73):
+            print(SCALE["11"])
+        elif wind_speed > 72:
+            print(SCALE["12"])
 
-    logging.debug(wind_speed)
+    except ValueError as error:
+        logging.debug(error)
+        print("Please enter a number.")
+        scale_wind()
 
 
 if __name__ == "__main__":
-    scale_wind("wind_speed")
+    sys.exit(scale_wind())
