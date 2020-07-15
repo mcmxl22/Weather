b1069bfaa1e2ffe17ec556e332d6c0fb63b5efb7
Micah M <mcmxl@reagan.com>
2020-07-15 15:52:39 -0600

    Redfactor if\else statements.
    Eliminated most if\elif\else statements by using a dictionary.
    Added use of numli().
    Added use of sys.exit()
    Added F-string formatting.
    Other minor improvements.

--- McWeather.py
+++ McWeather.py
@@ -1,59 +1,63 @@
 #!/usr/bin/env python3
 
 """
-index version 10.5
+index version 10.6
 Python 3.7
 """
 
+import sys
 from beaufort import scale_wind
 from celsius_conversion import to_fahrenheit
 from cloud_base import base
 from cloud_types import clouds
 from dew_point import dew_point
-from mcweather_entry_log import entry
 from fahrenheit_conversion import to_celsius
-from forecast import forecast
 from pressure_conversion import convert_millibars
 from wind_speed import wind
+from numli import add_numbers
+from pressure_forecast import forecast
 
 
-def prompt(prompt_choice):
+def main():
     """Prompt user to choose from list."""
     while True:
-        prompt_options = [
-            'Convert to Celsius', 'Convert to Fahrenheit',
-            'Find dew point', 'Weather forecast', 'Find cloud ceiling',
-            'Convert knots to MPH', 'Cloud types', 'Beaufort scale',
-            'Convert pressure', 'Exit']
+        weather_options = [
+            "Convert to Celsius",
+            "Convert to Fahrenheit",
+            "Find dew point",
+            "Find cloud ceiling",
+            "Convert knots to MPH",
+            "Cloud types",
+            "Beaufort scale",
+            "Forcast",
+            "Convert pressure",
+            "Exit",
+        ]
 
-        for index, prompt in enumerate(prompt_options, start=1):
-            print(index, prompt)
-        prompt_choice = input('\nChoose an option. ')
-        entry(prompt_choice)
+        add_numbers(weather_options)
 
-        if prompt_choice in '1':
-            to_celsius(prompt_choice)
-        elif prompt_choice in '2':
-            to_fahrenheit('prompt_choice')
-        elif prompt_choice in '3':
-            dew_point(prompt_choice)
-        elif prompt_choice in '4':
-            forecast(prompt_choice)
-        elif prompt_choice in '5':
-            base(prompt_choice)
-        elif prompt_choice in '6':
-            wind(prompt_choice)
-        elif prompt_choice in '7':
-            clouds(prompt_choice)
-        elif prompt_choice in '8':
-            scale_wind(prompt_choice)
-        elif prompt_choice in '9':
-            convert_millibars(prompt_choice)
-        elif prompt_choice in '10':
-            raise SystemExit
+        choice_dic = {
+            1: to_celsius,
+            2: to_fahrenheit,
+            3: dew_point,
+            4: base,
+            5: wind,
+            6: clouds,
+            7: scale_wind,
+            8: forecast,
+            9: convert_millibars,
+        }
+
+        try:
+            weather_choice = int(input("\nChoose an option. "))
+        except ValueError:
+            print("Please enter a number.")
+
+        if weather_choice == 10:
+            sys.exit(0)
         else:
-            print('\nInvalid Entry')
+            print(f"\n{choice_dic[weather_choice]()}\n")
 
 
 if __name__ == "__main__":
-    prompt('prompt_choice')
+    sys.exit(main())
