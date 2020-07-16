29aaca3f12edfacc50f0b9e2d8fc86cba32fc780
Micah M <mcmxl@reagan.com>
2020-07-16 00:58:12 -0600

    Added function plot_temp().

--- Temperature/plot_temp.py
+++ Temperature/plot_temp.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 """
-plot_temp.py version 1
+plot_temp.py version 1.1
 Python 3.7
 """
 
-
+import sys
 from matplotlib import pyplot as plt
 import xlrd
 
-wb = xlrd.open_workbook("C:/Users/User/MyStuff/Temp.xls")
-sheet1 = wb.sheet_by_name("Temperature")
 
-time_x = sheet1.col_values(1)
-temp_y = sheet1.col_values(0)
-plt.title("Temperature by time")
-plt.xlabel("Time")
-plt.ylabel("Temperature")
-plt.plot(time_x, temp_y)
+def plot_temp():
+    wb = xlrd.open_workbook("C:/Users/User/MyStuff/Temp.xls")
+    sheet1 = wb.sheet_by_name("Temperature")
 
-plt.show()
+    time_x = sheet1.col_values(1)
+    temp_y = sheet1.col_values(0)
+    plt.title("Temperature by time")
+    plt.xlabel("Time")
+    plt.ylabel("Temperature")
+    plt.plot(time_x, temp_y)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    sys.exit(plot_temp())
