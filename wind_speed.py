d282f37b1da13359e5594321af2143d3ca5b689f
Micah M <mcmxl@reagan.com>
2020-07-16 00:45:56 -0600

    Removed logging.

--- wind_speed.py
+++ wind_speed.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python3
+
 """
 wind_speed version 1.5
 Python 3.7
 """
 
-import logging
 import sys
-
-
-logging.basicConfig(filename="wind_speed.log", level=logging.DEBUG)
 
 
 def wind():
     """Convert knots to MPH"""
     try:
         convert_wind = int(input("Enter wind speed as knots. "))
-        logging.debug(convert_wind)
         conversion_formula = ((float(convert_wind)) * 6067) / 5280
         result = f"{convert_wind} knots is {round(conversion_formula, 2)} MPH."
-        logging.debug(result)
         print(result)
     except ValueError as error:
-        logging.debug(error)
         print("Entry must be a number")
         wind()
 
