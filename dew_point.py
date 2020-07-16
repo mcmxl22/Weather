7526492a4b0b109d71e6661c445660055e22fea7
Micah M <mcmxl@reagan.com>
2020-07-16 00:43:10 -0600

    Removed logging.

--- dew_point.py
+++ dew_point.py
@@ -5,11 +5,7 @@ dew_point version 1.4
 Python 3.7
 """
 
-import logging
 import sys
-
-
-logging.basicConfig(filename="dew_point.log", level=logging.DEBUG)
 
 
 def find_dew_point():
@@ -18,19 +14,13 @@ def find_dew_point():
 
     try:
         temperature = int(input("Enter the temperature in Celsius. "))
-        logging.debug(temperature)
         relative_humidity = int(input("Enter the relative humidity. "))
-        logging.debug(relative_humidity)
-
     except ValueError as error:
-        logging.debug(error)
         print("Entry must be a number")
-
     else:
         formula = temperature - ((100 - relative_humidity) / 5)
         result = f"The dew point is {formula}{degree_sign}C."
         print(result)
-        logging.debug(result)
 
 
 if __name__ == "__main__":
