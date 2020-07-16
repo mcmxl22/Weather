d4fb228153853f18b3f7b4af25150199c97a8027
Micah M <mcmxl@reagan.com>
2020-07-15 18:28:14 -0600

    Update McWeather.py

--- McWeather.py
+++ McWeather.py
@@ -9,13 +9,13 @@ import sys
 from beaufort import scale_wind
 from celsius_conversion import to_fahrenheit
 from cloud_base import find_base
-from cloud_types import clouds
-from dew_point import dew_point
+from cloud_types import main
+from dew_point import find_dew_point
 from fahrenheit_conversion import to_celsius
 from pressure_conversion import convert_millibars
 from wind_speed import wind
 from numli import add_numbers
-from pressure_forecast import forecast
+from pressure_forecast import get_forecast
 
 
 def main():
@@ -39,12 +39,12 @@ def main():
         choice_dic = {
             1: to_celsius,
             2: to_fahrenheit,
-            3: dew_point,
+            3: find_dew_point,
             4: find_base,
             5: wind,
-            6: clouds,
+            6: main,
             7: scale_wind,
-            8: forecast,
+            8: get_forecast,
             9: convert_millibars,
         }
 
