d541dedd510aa8b80a10620323f20bdb5789b9c6
Micah M <mcmxl@reagan.com>
2021-12-09 17:35:42 -0700

    Added conversion types.

--- wind_speed.py
+++ wind_speed.py
@@ -1,22 +1,84 @@
 #!/usr/bin/env python3
 
 """
-wind_speed version 1.5
+wind_speed version 1.6
 Python 3.7
 """
 
 
-def wind():
-    """Convert knots to MPH"""
-    try:
-        convert_wind = int(input("Enter wind speed as knots. "))
-        conversion_formula = ((float(convert_wind)) * 6067) / 5280
-        result = f"{convert_wind} knots is {round(conversion_formula, 2)} MPH."
-        print(result)
-    except ValueError:
-        print("Entry must be a number")
-        wind()
+class Wind:
+    """Convert wind speeds."""
+    def convert_to_mph(speed):
+        mph_conversion_formula = ((float(speed)) * 1.151)
+        mph_result = mph_conversion_formula
+        return mph_result
+    
+    def convert_to_knots(speed):
+        knots_conversion_formula = ((float(speed)) / 1.151)
+        knots_result = knots_conversion_formula
+        return knots_result
+    
+    def convert_to_kilometers(speed):
+        kilometers_conversion_formula = ((float(speed)) * 0.6214)
+        kilometers_result = kilometers_conversion_formula
+        return kilometers_result
+
+
+class Menu:
+    """Prepare a menu."""
+    def add_numbers(num):
+        """Add numbers to the menu list."""
+        for c, value in enumerate(num, 1):
+            print(c, value)
+
+
+    def list_choices():
+        """Give user a choice of actions."""
+        choose_conversion = ["Miles to Knots", "Knots to Miles", "Miles to Kilometers", "Exit"]
+        return Menu.add_numbers(choose_conversion)
+
+
+def main():
+    list_choices = Menu.list_choices()
+    error = "Please enter a valid speed."
+
+    while True:
+        list_choices
+        action_choice = input("What do you want to do? ")
+        
+        if action_choice == '1':
+            try:
+                enter_speed = int(input(f"Enter wind speed as MPH. "))
+                conversion_result = round(Wind.convert_to_knots(enter_speed), 2)
+                print(f"{enter_speed} MPH is {conversion_result} Knots.")
+
+            except ValueError:
+                print(error)
+                main()
+        
+        elif action_choice == '2':
+            try:
+                enter_speed = int(input(f"Enter wind speed as knots. "))
+                conversion_result = round(Wind.convert_to_mph(enter_speed), 2)
+                print(f"{enter_speed} knots is {conversion_result} MPH.")
+
+            except ValueError:
+                print(error)
+                main()
+
+        elif action_choice == '3':
+            try:
+                enter_speed = int(input(f"Enter wind speed as MPH. "))
+                conversion_result = round(Wind.convert_to_kilometers(enter_speed), 2)
+                print(f"{enter_speed} MPH is {conversion_result} KPH.")
+
+            except ValueError:
+                print(error)
+                main()
+
+        elif action_choice == '4':
+            quit()
 
 
 if __name__ == "__main__":
-    exit(wind())
+    main()
