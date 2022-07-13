b0af6457a330b838dae523698aeb95407de93f1d
M. McConnaughey <mcmxl@reagan.com>
2022-07-13 12:17:03 -0600

    Minor changes.

--- Temperature/get_time.py
+++ Temperature/get_time.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 """
-get_time.py version 1.1
-Python 3.7
+Author: M. McConnaughey
+get_time.py version 1.2
+Date: 07/13/2022
+Python 3.7+
 """
 
-import sys
 from datetime import datetime
 
 
 def get_time():
-    """Gets time of day."""
+    """Get time of day."""
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     return current_time
 
 
 if __name__ == "__main__":
-    sys.exit(get_time())
+    get_time()
