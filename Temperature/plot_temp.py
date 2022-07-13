48cc78d8418727ff9ddd9932f8fa21c33f1041c8
M. McConnaughey <mcmxl@reagan.com>
2022-07-13 12:22:06 -0600

    Add docstring. Minor changes.

--- Temperature/plot_temp.py
+++ Temperature/plot_temp.py
@@ -1,28 +1,28 @@
 #!/usr/bin/env python3
 
 """
-plot_temp.py version 1.1
-Python 3.7
+Author: M. McConnaughey
+plot_temp.py version 1.2
+Date: 07/13/2022
+Python 3.7+
 """
 
-import sys
 from matplotlib import pyplot as plt
 import xlrd
 
 
 def plot_temp():
-    wb = xlrd.open_workbook("C:/Users/User/MyStuff/Temp.xls")
+    """Plot the temperature from the .xls file."""
+    wb = xlrd.open_workbook("Temp.xls")
     sheet1 = wb.sheet_by_name("Temperature")
-
     time_x = sheet1.col_values(1)
     temp_y = sheet1.col_values(0)
-    plt.title("Temperature by time")
+    plt.title("Time")
     plt.xlabel("Time")
     plt.ylabel("Temperature")
     plt.plot(time_x, temp_y)
-
     plt.show()
 
 
 if __name__ == "__main__":
-    sys.exit(plot_temp())
+    plot_temp()
