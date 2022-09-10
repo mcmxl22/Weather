4cb9267d431e16f78b7f39a31195d5190f59aae2
M. McConnaughey <mcmxl@reagan.com>
2022-09-09 21:03:20 -0600

    Update cloud_base.py

--- cloud_base.py
+++ cloud_base.py
@@ -2,9 +2,9 @@
 
 """
 Author: M McConnaughey
-cloud_base version 1.5
+cloud_base version 1.6
 Python 3.7
-Date: 07/12/2021
+Date: 08/09/2022
 """
 
 
@@ -14,21 +14,19 @@ def find_base():
     """
     spread = get_temperature() - get_dewpoint()
     formula = spread / 2.5 * 1000
-    print(f"The cloud ceiling is {formula} feet above the ground.")
+    return formula
 
 
 def get_dewpoint():
     enter_dew_point = int(input("Enter dew point in Celsius. "))
     return enter_dew_point
 
+
 def get_temperature():
     temperature = int(input("Enter temperature in Celsius. "))
     return temperature
 
 
-def main():
-    find_base()
-
-
 if __name__ == "__main__":
-    main()
+    base = find_base()
+    print(f"The cloud ceiling is {base} feet above the ground.")
