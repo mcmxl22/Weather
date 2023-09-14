280f46c80aa2b2a1800b2c619a2133eeefb485ed
M. McConnaughey <mcmxl@hotmail.com>
2023-09-14 13:08:03 -0600

    Add data class and other improvements

--- wind_speed.py
+++ wind_speed.py
@@ -1,87 +1,79 @@
-#!/usr/bin/env python3
-
 """
 Author: M. McConnaughey
-wind_speed version 1.7
-Date: 07/13/2022
+wind_speed version 1.8
+Date: 08/14/2023
 Python 3.7
 """
 
+from dataclasses import dataclass
 
-class Wind:
-    """Convert wind speeds."""
-    def convert_to_mph(speed):
-        mph_conversion_formula = ((float(speed)) * 1.151)
-        mph_result = mph_conversion_formula
-        return mph_result
-    
-    def convert_to_knots(speed):
-        knots_conversion_formula = ((float(speed)) / 1.151)
-        knots_result = knots_conversion_formula
-        return knots_result
-    
-    def convert_to_kilometers(speed):
-        kilometers_conversion_formula = ((float(speed)) * 0.6214)
-        kilometers_result = kilometers_conversion_formula
-        return kilometers_result
+@dataclass(frozen=True)
+class ConversionCoefficients:
+    """Conversion Coefficients."""
+    mph_to_knots: float = 0.868976242
+    knots_to_mph: float = 1.1507794480235425
+    mph_to_kph: float = 1.60934
 
 
-class Menu:
-    """Prepare a menu."""
-    def add_numbers(num):
-        """Add numbers to the menu list."""
-        for c, value in enumerate(num, 1):
-            print(c, value)
+def mph_to_knots(mph: float) -> float:
+    """Convert miles per hour to knots per hour."""
+    return mph * ConversionCoefficients.mph_to_knots
 
+def knots_to_mph(knots: float) -> float:
+    """Convert knots per hour to miles per hour."""
+    return knots * ConversionCoefficients.knots_to_mph
 
-    def list_choices():
-        """Give user a choice of actions."""
-        choose_conversion = ["Miles to Knots", "Knots to Miles", "Miles to Kilometers", "Exit"]
-        return Menu.add_numbers(choose_conversion)
+def mph_to_kph(mph: float) -> float:
+    """Convert miles per hour to kilometers per hour."""
+    return mph * ConversionCoefficients.mph_to_kph
 
+def add_numbers(num):
+    """Add numbers to the menu list."""
+    for c, value in enumerate(num, 1):
+        print(c, value)
+
+def list_choices():
+    """Give user a choice of actions."""
+    choose_conversion = ["Miles to Knots", "Knots to Miles", "Miles to Kilometers", "Exit"]
+    return add_numbers(choose_conversion)
 
 def main():
     """main function"""
-    list_choices = Menu.list_choices()
     error = "Please enter a valid speed."
 
     while True:
-        list_choices
-        action_choice = input("What do you want to do? ")
-        
-        if action_choice == '1':
-            try:
-                enter_speed = int(input(f"Enter wind speed as MPH. "))
-                conversion_result = round(Wind.convert_to_knots(enter_speed), 2)
-                print(f"{enter_speed} MPH is {conversion_result} Knots.")
+        list_choices()
+        choice = input("What do you want to do? ")
 
+        if choice == '1':
+            try:
+                speed = int(input("Enter wind speed in MPH. "))
             except ValueError:
                 print(error)
-                main()
-        
-        elif action_choice == '2':
+            else:
+                result = mph_to_knots(speed)
+                print(f"{speed} MPH is {round(result, 2)} Knots.")
+
+        elif choice == '2':
             try:
                 enter_speed = int(input(f"Enter wind speed as knots. "))
-                conversion_result = round(Wind.convert_to_mph(enter_speed), 2)
-                print(f"{enter_speed} knots is {conversion_result} MPH.")
+                result = round(knots_to_mph(enter_speed), 2)
+                print(f"{enter_speed} knots is {round(result, 2)} MPH.")
 
             except ValueError:
                 print(error)
-                main()
 
-        elif action_choice == '3':
+        elif choice == '3':
             try:
                 enter_speed = int(input(f"Enter wind speed as MPH. "))
-                conversion_result = round(Wind.convert_to_kilometers(enter_speed), 2)
-                print(f"{enter_speed} MPH is {conversion_result} KPH.")
+                result = round(mph_to_kph(enter_speed), 2)
+                print(f"{enter_speed} MPH is {round(result, 2)} KPH.")
 
             except ValueError:
                 print(error)
-                main()
 
-        elif action_choice == '4':
-            quit()
-
+        elif choice == '4':
+            break
 
 if __name__ == "__main__":
     main()
