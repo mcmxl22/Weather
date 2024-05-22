8e32779a0194298806ec69ce86641b8bdc9b82d7
M. McConnaughey <mcmxl@hotmail.com>
2024-05-22 18:45:55 +0000

    function call fixes

--- McWeather.py
+++ McWeather.py
@@ -11,11 +11,10 @@ from beaufort import scale_wind
 from celsius_conversion import to_fahrenheit
 from cloud_base import find_base
 from cloud_types import cloud_main
-from dew_point import find_dew_point
+from dew_point import calculate_dew_point
 from fahrenheit_conversion import to_celsius
 from pressure_conversion import convert_millibars
-from wind_speed import wind
-from numli import add_numbers
+from wind_speed import main
 from pressure_forecast import get_forecast
 
 
@@ -35,14 +34,15 @@ def main():
             "Exit",
         ]
 
-        add_numbers(weather_options)
+        for c, value in enumerate(weather_options, 1):
+            print(c, value)
 
         choice_dic = {
             1: to_celsius,
             2: to_fahrenheit,
-            3: find_dew_point,
+            3: calculate_dew_point,
             4: find_base,
-            5: wind,
+            5: main,
             6: cloud_main,
             7: scale_wind,
             8: get_forecast,
